2f161d0f5aa920cc04c42d635e45dbafd7d60875
Athar <mohd.rule123@gmail.com>
2022-06-04 11:18:36 +0530

    added account initialisation functionality

--- programs/solana_file_sharing_system/src/lib.rs
+++ programs/solana_file_sharing_system/src/lib.rs
@@ -7,9 +7,22 @@ pub mod solana_file_sharing_system {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let share = &mut ctx.accounts.files;
+        share.docs[0] = 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init,payer=user,space=264)]
+    pub files: Account<'info,Files>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program : Program<'info,System>
+}
+
+#[account]
+pub struct Files{
+    pub docs : [u8;1024] //just for testing , will change later
+}

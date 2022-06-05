5c9a5526eaec7faef2842e612b9cbe75f63a7944
Athar <mohd.rule123@gmail.com>
2022-06-05 22:55:39 +0530

    integrating wallet into client

---
+++ app/src/App.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getLedgerWallet,
+  getPhantomWallet,
+  getSlopeWallet,
+  getSolflareWallet,
+  getSolletExtensionWallet,
+  getSolletWallet,
+  getTorusWallet,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import MyWallet from "./MyWallet";
+
+function App() {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint
+  const endpoint = React.useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
+  // Only the wallets you configure here will be compiled into your application
+  const wallets = React.useMemo(
+    () => [
+      getPhantomWallet(),
+      getSlopeWallet(),
+      getSolflareWallet(),
+      getLedgerWallet(),
+      getSolletWallet({ network }),
+      getSolletExtensionWallet({ network }),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <div className="App">
+          <header className="App-header">
+            Solana File Sharing System
+            <MyWallet />
+          </header>
+        </div>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;
